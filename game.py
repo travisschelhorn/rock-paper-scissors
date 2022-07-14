4ef9f0e2a953938487b4e7b9b65189a4e8756de7
travisschelhorn <tjs7235@stern.nyu.edu>
2022-07-13 22:37:03 -0400

    Added Determining the Winner

--- game.py
+++ game.py
@@ -2,7 +2,6 @@
 
 from ast import If, NotIn
 
-
 print("Rock, Paper, Scissors")
 
 print("Welcome to the Rock Paper Scissors Game")
@@ -17,14 +16,12 @@ user_selection = user_selection.lower()
 
 print("your choice: ",(user_selection))
 
-
 # Options 
 options = ["rock", "paper", "scissors"]
 
 if user_selection not in options:
     print("Sorry. Your choice is invalid")
     exit()
-
 
 # Computer Selection
 
@@ -36,8 +33,27 @@ computer = (random.choice(options))
 print("Computer chose", (computer))
 
 # Determining the Winner
-
-
+# Rock Options
+if user_selection == "rock" and computer == "rock":
+    print ("It's a tie!")
+elif user_selection == "rock" and computer == "paper":
+    print ("The computer won. Better luck next time")
+elif user_selection == "rock" and computer == "scissors":
+    print ("Congratulations! You win!")
+# Paper Options
+elif user_selection == "paper" and computer == "rock":
+    print ("Congratulations! You win!!")
+elif user_selection == "paper" and computer == "paper":
+    print ("It's a tie!")
+elif user_selection == "paper" and computer == "scissors":
+    print ("The computer won. Better luck next time")
+# Scissor Options
+elif user_selection == "scissors" and computer == "rock":
+    print ("The computer won. Better luck next time")
+elif user_selection == "scissors" and computer == "paper":
+    print ("Congratulations! You win!")
+elif user_selection == "scissors" and computer == "scissors":
+    print ("It's a tie!")
 
 # End of Game
-print("This is the end of the game. Thank you for playing")
+print("This is the end of the game. Thank you for playing.")
